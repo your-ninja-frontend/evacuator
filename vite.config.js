4caa53087549de1528ae949e8903ecaf1c55b338
ninja_frontend <stifffffler102@gmail.com>
2024-09-29 23:22:49 +0300

    test build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
+	base: '/evacuator/',
 	build: {
 		outDir: 'docs',
 		rollupOptions: {
